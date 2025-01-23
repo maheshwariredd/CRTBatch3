e9e6d22f17043ba5e49c571c855026f3b0c801ff
maheshwariredd <gunreddyakihlreddy555@gmail.com>
2025-01-23 18:06:22 +0530

    message is sending to git

---
+++ dom.js
@@ -0,0 +1,10 @@
+console.log(window)
+console.log(document)
+
+let element = document.getElementById("demo");
+element.InnerText="G+"
+console.log(element);
+
+let ele = document.getElementById("test");
+ele.InnerHTML = "<h1>header</h1>"
+console.log(ele)
