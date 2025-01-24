572ecb41240ea036e04a554259ceac461d0a41a2
maheshwariredd <gunreddyakihlreddy555@gmail.com>
2025-01-24 19:14:54 +0530

    message to the github

--- async.js
+++ async.js
@@ -1,62 +1,87 @@
 
-console.log(20);
-console.log(30);
-console.l0g(40);
-console.log(50);
-console.log(60);
+// console.log(20);
+// console.log(30);
+// console.l0g(40);
+// console.log(50);
+// console.log(60);
 
 // setTimeOut methood
 
-console.log(20);
-console.log(30);
-setTimeOut(()=>{
-    console.l0g(40);
-},500)
-console.log(50);
-console.log(60);
+// console.log(20);
+// console.log(30);
+// setTimeOut(()=>{
+//     console.l0g(40);
+// },500)
+// console.log(50);
+// console.log(60);
 
 // setIntervel
 
-console.log(20);
-console.log(30);
-setInterval(()=>{ 
-    console.l0g(40);
-},1500)
-console.log(50);
-console.log(60);
+// console.log(20);
+// console.log(30);
+// setInterval(()=>{ 
+//     console.l0g(40);
+// },1500)
+// console.log(50);
+// console.log(60);
 
 //promise
 
-let p2 = new Promise((  resolved , reject) => { 
-    resolved("successfull");
+// let p2 = new Promise((  resolved , reject) => { 
+//     resolved("successfull");
 
-})
-p2.then((response)=> console.log(response))
-p2.catch((error)=> console.log(error))
-p2.finally(()=> console.log("finally for both "))
+// })
+// p2.then((response)=> console.log(response))
+// p2.catch((error)=> console.log(error))
+// p2.finally(()=> console.log("finally for both "))
 
 
-function fetchUsers(){
-    let response = fetch ("https://jsonplaceholder.typicode.com/users");
-    response.then(result=>{
-        return result.json().then(data=>{
-            console.log(data);
-            let store = daocument.getElementById("store");
-            console.log(store);
-            data.map(user=>{
+// function fetchUsers(){
+//     let response = fetch ("https://jsonplaceholder.typicode.com/users");
+//     response.then(result=>{
+//         return result.json().then(data=>{
+//             console.log(data);
+//             let store = daocument.getElementById("store");
+//             console.log(store);
+//             data.map(user=>{
                 
-                store.innerHtml +=`
-                <tr>
-                <td>${user.id}</td>
-                <td>${user.name}</td>
-                <td>${user.email}</td>
-                <td>${user.company.name}</td>
-                </tr>
-                `
-            })
-        })
-    })
-    .catch(err=>console.log(err))
+//                 store.innerHtml +=`
+//                 <tr>
+//                 <td>${user.id}</td>
+//                 <td>${user.name}</td>
+//                 <td>${user.email}</td>
+//                 <td>${user.company.name}</td>
+//                 </tr>
+//                 `
+//             })
+//         })
+//     })
+//     .catch(err=>console.log(err))
 
+// }
+// fetchUsers();
+
+ // async and await
+ 
+let p = new Promise((resolve , reject)=>{
+    setTimeout(()=>{
+        resolve("succes");
+
+    }, 5000)
+})
+
+async function demo(){
+    console.log("Start");
+    let x= await p;
+    console.log(x);
+    console.log("End");
+
+};
+demo()
+
+async function fetchUsers(){
+    let response = await fetch("https://jsonplaceholder.typicode.com/users");
+    let data = await response.json();
+    console.log(data);
 }
 fetchUsers();
--- dom.js
+++ dom.js
@@ -1,10 +1,28 @@
-console.log(window)
-console.log(document)
+// console.log(window)
+// console.log(document)
 
-let element = document.getElementById("demo");
-element.InnerText="G+"
-console.log(element);
+// let element = document.getElementById("demo");
+// element.InnerText="G+"
+// console.log(element);
 
-let ele = document.getElementById("test");
-ele.InnerHTML = "<h1>header</h1>"
-console.log(ele)
+// let ele = document.getElementById("test");
+// ele.InnerHTML = "<h1>header</h1>"
+// console.log(ele)
+
+// let ele = document.getElementsByClassNameName("test");
+// console.log(ele);
+// console.log(Array.isArray(ele));
+// ele[0].innerText="Hello"
+
+// //ele[0].style.backgroundColor="tomato"
+
+// //? syntax :- [...variable];
+// let x = [...ele];
+// //console.log(x , Array.isArray(x));
+// x.map(element=>{
+//     //console.log(element);
+//     element.style.backgroundColor="orange"
+// })
+
+// let ele = document.querySelectorAll(".test");
+// console.log(ele);
