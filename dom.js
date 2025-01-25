09358cbedd2abf8c5f0b2f35ee61236db576dfdb
maheshwariredd <gunreddyakihlreddy555@gmail.com>
2025-01-25 12:59:33 +0530

    message sending to git

--- dom.js
+++ dom.js
@@ -26,3 +26,35 @@
 
 // let ele = document.querySelectorAll(".test");
 // console.log(ele);
+
+
+// let ele = document.createElement("h1");
+// ele.innerText = "Dynamic creation of html element";
+// ele.id = "demo";
+// ele.removeAttribute("id")
+//console.log(ele)
+// let image = document.createElement("img");
+// image.src = "./burger.jpg";
+// console.log(image);
+
+// document.body.appendChild(ele);
+// document.body.appentChild(image)
+ 
+
+let form = document.querySelector("form");
+let username = document.getElementById("uName");
+let mail = document.getElementById("uMail");
+let psw = document.getElementById("uPass");
+ 
+
+form.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    let uname= username.value;
+    let uemail= mail.value;
+    let upass= psw.value;
+    let userDetails={
+        uname,uemail,upass
+    }
+    console.log(userDetails);
+    localStorage.setItem("userData", JSON.stringify(userDetails))
+})
