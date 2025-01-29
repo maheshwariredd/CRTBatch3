2f1a5d2ef732d1e9ae15f9612deeca6d0d332835
maheshwariredd <gunreddyakihlreddy555@gmail.com>
2025-01-29 22:25:41 +0530

    message

---
+++ arr.js
@@ -0,0 +1,11 @@
+let userDetails={
+    emp_name:"G.maheshwari",
+    emp_id:123,
+    emp_company:"google",
+    emp_salary:250000,
+    emp_address:{
+        emp_city:"hyd",
+        emp_area:"balnagar"
+    }
+}
+console.log(userDetails.emp_address.emp_city);
